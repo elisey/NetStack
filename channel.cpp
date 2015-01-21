37210770a7ab4358d35cdff9a7ea8a281d158f7f
Elisey <elisey.rav@gmail.com>
2015-01-21 17:09:09 +0700

    В базовом классе Channel теперь нет задачи на отправку. Отправка выполняется в контексте вызывающей задачи.

--- channel.cpp
+++ channel.cpp
@@ -1,27 +1,18 @@
 #include "channel.h"
 #include "debug.h"
 
-void Channel_TxTask(void *param);
-
 Channel::Channel()
 {
-	txQueue = xQueueCreate(10, sizeof(Frame));
 	rxQueue = xQueueCreate(10, sizeof(Frame));
-
-	xTaskCreate(
-			Channel_TxTask,
-			"Channel_TxTask",
-			200,
-			this,
-			tskIDLE_PRIORITY + 1,
-			NULL);
 }
 
-void Channel::send(const Frame *ptrFrame)
+void Channel::send(Frame *ptrFrame)
 {
-	BaseType_t result;
-	result = xQueueSend(txQueue, ptrFrame, (TickType_t)portMAX_DELAY);
-	assert(result == pdPASS);
+	transfer(ptrFrame);
+
+	while(isTxBusy() == true)	{
+		vTaskDelay(1 / portTICK_RATE_MS);
+	}
 }
 
 bool Channel::receive(Frame *ptrFrame, unsigned int timeout)
@@ -32,30 +23,6 @@ bool Channel::receive(Frame *ptrFrame, unsigned int timeout)
 		return true;
 	}
 	return false;
-}
-
-void Channel_TxTask(void *param)
-{
-	Channel *ptrObj = static_cast<Channel*>(param);
-	ptrObj->txTask();
-	while(1);
-}
-
-void Channel::txTask()
-{
-	while(1)
-	{
-		Frame frame;
-		BaseType_t result;
-		result = xQueueReceive(txQueue, &(frame), (portMAX_DELAY));
-		assert(result == pdPASS);
-
-		transfer(&frame);
-
-		while(isTxBusy() == true)	{
-			vTaskDelay(1 / portTICK_RATE_MS);
-		}
-	}
 }
 
 void Channel::handleRxPacket(Frame *ptrFrame)
--- channel.h
+++ channel.h
@@ -10,21 +10,19 @@ class Channel
 {
 public:
 	Channel();
-	void send(const Frame *ptrFrame);
+	void send(Frame *ptrFrame);
 	bool receive(Frame *ptrFrame, unsigned int timeout);
-	virtual bool isTxBusy() = 0;
+
 	virtual void startRx() = 0;
 	virtual void stopRx() = 0;
-	void txTask();
 
 protected:
 	virtual void transfer(Frame *ptrFrame) = 0;
-
+	virtual bool isTxBusy() = 0;
 	void handleRxPacket(Frame *ptrFrame);
 
 	Frame rxFrame;
 
 private:
-	QueueHandle_t txQueue;
 	QueueHandle_t rxQueue;
 };
