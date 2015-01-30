4ec05defc87c806d0bdea33c01b25e3af7ce38dc
Elisey <elisey.rav@gmail.com>
2015-01-30 11:24:41 +0700

    Заготовка определений для реализация таймеров на базе FreeRTOS.

--- FreeRTOS/include/FreeRTOSConfig.h
+++ FreeRTOS/include/FreeRTOSConfig.h
@@ -96,6 +96,11 @@
 #define configUSE_RECURSIVE_MUTEXES		1
 #define configUSE_COUNTING_SEMAPHORES	1
 
+#define configUSE_TIMERS				0
+#define configTIMER_TASK_PRIORITY		(tskIDLE_PRIORITY + 1)
+#define configTIMER_QUEUE_LENGTH		(8)
+#define configTIMER_TASK_STACK_DEPTH	(configMINIMAL_STACK_SIZE)
+
 /* Co-routine definitions. */
 #define configUSE_CO_ROUTINES			0
 #define configMAX_CO_ROUTINE_PRIORITIES ( 2 )
