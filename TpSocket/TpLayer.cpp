9aa6198c658a0f3ade23203b12f91c7c0c9a5780
Elisey <elisey.rav@gmail.com>
2015-02-27 18:20:13 +0700

    Добавлен класс регистрации сокетов на порты

---
+++ TpSocket/TpLayer.cpp
@@ -0,0 +1,118 @@
+#include "TpLayer.h"
+#include "TpFrame.h"
+#include "debug.h"
+
+TpLayer::TpLayer()
+{
+
+}
+
+int TpLayer::registerSocket(TpSocket* ptrPtSocket, uint8_t port)
+{
+	if (port == 0)	{
+		port = getFreePortNumber();
+	}
+	if (isPortOccupied(port) == true)	{
+		return (-1);
+	}
+	int index;
+	index == findFreeNode();
+	assert(index != (-1));
+	nodes[index].ptrPtSocket = ptrPtSocket;
+	nodes[index].port = port;
+	return port;
+}
+
+bool TpLayer::unregisterSocket(TpSocket *ptrPtSocket, uint8_t port)
+{
+	assert(port != 0);
+
+	if (isPortOccupied(port) == false)	{
+		return false;
+	}
+
+	int index;
+	index = findNodeIndexByPort(port);
+
+	if (nodes[index].ptrPtSocket == ptrPtSocket)	{
+		clearNodeByIndex(index);
+		return true;
+	}
+	return false;
+}
+
+void TpLayer::handleTpFrame(NpFrame *ptrNpFrame)
+{
+	uint16_t srcAddress = ptrNpFrame->getSrcAddress();
+	TpFrame tpFrame;
+	tpFrame.clone(*ptrNpFrame);
+	tpFrame.srcAddress = srcAddress;
+
+	uint8_t dstPort = tpFrame.getDstPort();
+
+	int index;
+	index = findNodeIndexByPort(dstPort);
+	if (index == (-1))	{
+		tpFrame.free();
+		return;
+	}
+	if (nodes[index].ptrPtSocket != NULL)	{
+		nodes[index].ptrPtSocket->handleFrame(&tpFrame);	//TODO
+	}
+}
+
+int TpLayer::getFreePortNumber()
+{
+	static int port = DYNAMIC_START_PORT_INDEX;
+
+	int counter = 0;
+	while( isPoisPortOccupied(port) == true )	{
+		port++;
+		if (port == 0)	{
+			port = DYNAMIC_START_PORT_INDEX;
+		}
+		counter++;
+		if (counter >= 0xff - DYNAMIC_START_PORT_INDEX)	{
+			return (-1);
+		}
+	}
+	port++;
+	return port;
+}
+
+bool TpLayer::isPortOccupied(uint8_t port)
+{
+	int index = findNodeIndexByPort(port);
+
+	if (index != (-1) )	{
+		return true;
+	}
+	return false;
+}
+
+int TpLayer::findNodeIndexByPort(uint8_t port)
+{
+	int i;
+	for (i = 0; i < MAX_NUM_OF_SOCKETS; ++i) {
+		if (nodes[i].port == port)	{
+			return i;
+		}
+	}
+	return (-1);
+}
+
+int TpLayer::findFreeNode()
+{
+	int i;
+	for (i = 0; i < MAX_NUM_OF_SOCKETS; ++i) {
+		if (nodes[i].ptrPtSocket == NULL)	{
+			return i;
+		}
+	}
+	return (-1);
+}
+
+void TpLayer::clearNodeByIndex(uint8_t index)
+{
+	nodes[index].clear();
+}
---
+++ TpSocket/TpLayer.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include "TpSocket.h"
+#include "NpFrame.h"
+
+#define MAX_NUM_OF_SOCKETS			(10)
+#define DYNAMIC_START_PORT_INDEX	(128)
+
+struct RegisteredSocketNode {
+	RegisteredSocketNode()
+	:	ptrPtSocket(NULL), port(0)
+	{
+	}
+
+	void clear()
+	{
+		ptrPtSocket = NULL;
+		port = 0;
+	}
+	TpSocket *ptrPtSocket;
+	uint8_t port;
+};
+
+
+class TpLayer	{
+public:
+
+	int registerSocket(TpSocket *ptrPtSocket, uint8_t port = 0);
+	bool unregisterSocket(TpSocket *ptrPtSocket, uint8_t port);
+	void handleTpFrame(NpFrame *ptrNpFrame);
+
+private:
+	int getFreePortNumber();
+	bool isPortOccupied(uint8_t port);
+	int findNodeIndexByPort(uint8_t port);
+	int findFreeNode();
+	void clearNodeByIndex(uint8_t index);
+
+	RegisteredSocketNode nodes[MAX_NUM_OF_SOCKETS];
+
+//Singleton realisation
+public:
+	static TpLayer& instance()
+	{
+		static TpLayer theSingleInstance;
+		return theSingleInstance;
+	}
+private:
+	TpLayer();
+	TpLayer(const TpLayer& root);
+	TpLayer& operator=(const TpLayer&);
+};
