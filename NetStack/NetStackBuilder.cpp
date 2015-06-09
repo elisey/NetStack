66ec41aeae32fd9f24ff6316ccc20ad0c02bb0a6
Elisey <elisey.rav@gmail.com>
2015-06-09 16:22:45 +0700

    Исправлены настройки сетевого стека

--- NetStack/NetStackBuilder.cpp
+++ NetStack/NetStackBuilder.cpp
@@ -6,62 +6,86 @@
 
 #include "ncmp_layer_slave.h"
 #include "ncmp_layer_master.h"
-#include "MacLayerNrf.h"
 
+/*
+ * Interface 0	-	UART2 (Slave interface)
+ * Interface 1	-	UART3
+ * Interface 2	-	UART1
+ * Interface 3	-	UART4
+ * Interface 4	-	UART5 (Interrupts)
+ */
 
-#ifdef MASTER
-MacLayerSFBus mc1(&ch1);
-//MacLayerSFBus mc2(&channelUartIrq);
-MacLayerSFBus mc2(&ch3);
+MacLayerSFBus mc0(&ch2);
+MacLayerSFBus mc1(&ch3);
+MacLayerSFBus mc2(&ch1);
+MacLayerSFBus mc3(&ch4);
+MacLayerSFBus mc4(&channelUartIrq);
 
-NpLayer np1(&mc1, 0);
-NpLayer np2(&mc2, 1);
+NpLayer np0(&mc0, 0);
+NpLayer np1(&mc1, 1);
+NpLayer np2(&mc2, 2);
+NpLayer np3(&mc3, 3);
+NpLayer np4(&mc4, 4);
 
-NpLayer *interfaces[NUM_OF_INTERFACES] = {&np1, &np2};
+NpLayer *interfaces[NUM_OF_INTERFACES] = {&np0, &np1, &np2, &np3, &np4};
 
-NcmpLayerSlave ncmp1(&np1, interfaceType_PointToPoint);
+NcmpLayerSlave ncmp0(&np0, interfaceType_PointToPoint);
+
+NcmpLayerMaster ncmp1(&np1, interfaceType_PointToPoint);
 NcmpLayerMaster ncmp2(&np2, interfaceType_PointToPoint);
-
-
-#endif
-#ifdef BRIDGE
-
-MacLayerSFBus mc1(&ch1);
-MacLayerNrf mcRadio;
-
-NpLayer np1(&mc1, 0);
-NpLayer np2(&mcRadio, 1);
-
-NpLayer *interfaces[NUM_OF_INTERFACES] = {&np1, &np2};
-
-NcmpLayerSlave ncmp1(&np1, interfaceType_PointToPoint);
-NcmpLayerMaster ncmp2(&np2, interfaceType_Star);
-#endif
-
-#ifdef SLAVE
-
-MacLayerNrf mcRadio;
-
-NpLayer np1(&mcRadio, 0);
-
-NpLayer *interfaces[NUM_OF_INTERFACES] = {&np1};
-
-NcmpLayerSlave ncmp1(&np1, interfaceType_Star);
-
-#endif
-
+NcmpLayerMaster ncmp3(&np3, interfaceType_PointToPoint);
+NcmpLayerMaster ncmp4(&np4, interfaceType_PointToPoint);
 
 TpSocket tpSocket1;
-TpSocket tpSocket2;
-TpSocket tpSocket3;
-TpSocket tpSocket4;
+
+Led led1(GPIOB, GPIO_Pin_7, false);
+Led led2(GPIOB, GPIO_Pin_8, false);
+Led led3(GPIOB, GPIO_Pin_9, false);
+
+Led led4(GPIOC, GPIO_Pin_13, false);
+Led led5(GPIOC, GPIO_Pin_14, false);
+
+Pin pinInterface1_PoeStatusPin(GPIOB, GPIO_Pin_14, pinDirection_Input);
+Pin pinInterface1_PoeControlPin(GPIOB, GPIO_Pin_15, pinDirection_Output);
+
+Pin pinInterface2_PoeStatusPin(GPIOC, GPIO_Pin_9, pinDirection_Input);
+Pin pinInterface2_PoeControlPin(GPIOA, GPIO_Pin_8, pinDirection_Output);
+
+Pin pinInterface3_PoeStatusPin(GPIOA, GPIO_Pin_12, pinDirection_Input);
+Pin pinInterface3_PoeControlPin(GPIOA, GPIO_Pin_15, pinDirection_Output);
+
+Pin pinInterface4_PoeStatusPin(GPIOB, GPIO_Pin_5, pinDirection_Input);
+Pin pinInterface4_PoeControlPin(GPIOB, GPIO_Pin_6, pinDirection_Output);
+
+InterfaceControl_SFBusSlave interfaceControlSlave(
+		&ncmp0,
+		&led5);
+
+InterfaceControl_SFBusMaster interfaceControlMaster1(
+		&ncmp1,
+		&led4,
+		&pinInterface1_PoeStatusPin,
+		&pinInterface1_PoeControlPin);
+
+InterfaceControl_SFBusMaster interfaceControlMaster2(
+		&ncmp2,
+		&led3,
+		&pinInterface2_PoeStatusPin,
+		&pinInterface2_PoeControlPin);
+
+InterfaceControl_SFBusMaster interfaceControlMaster3(
+		&ncmp3,
+		&led2,
+		&pinInterface3_PoeStatusPin,
+		&pinInterface3_PoeControlPin);
+
+InterfaceControl_SFBusMaster interfaceControlMaster4(
+		&ncmp4,
+		&led1,
+		&pinInterface4_PoeStatusPin,
+		&pinInterface4_PoeControlPin);
+
 void Interfaces_Init(uint16_t address)
 {
-#ifdef BRIDGE
-	mcRadio.init(address);
-#endif
-#ifdef SLAVE
-	mcRadio.init(address);
-#endif
 	selfAddress = address;
 }
--- NetStack/NetStackBuilder.h
+++ NetStack/NetStackBuilder.h
@@ -1,37 +1,17 @@
-#ifndef CONFIG_H_
-#define CONFIG_H_
+#pragma once
 
 #include <stdint.h>
 
 #include "np_layer.h"
 #include "TpSocket.h"
 
-#define DEVICE_TYPE	2
+#include "InterfaceControl_SFBusSlave.h"
+#include "InterfaceControl_SFBusMaster.h"
 
-#if (DEVICE_TYPE == 1)
-#define MASTER
-#elif (DEVICE_TYPE == 2)
-#define BRIDGE
-#elif (DEVICE_TYPE == 3)
-#define SLAVE
-#endif
-
-#ifdef MASTER
-#define NUM_OF_INTERFACES	2
-#endif
-#ifdef BRIDGE
-#define NUM_OF_INTERFACES	2
-#endif
-#ifdef SLAVE
-#define NUM_OF_INTERFACES	1
-#endif
-
+#define NUM_OF_INTERFACES	5
 
 extern NpLayer *interfaces[NUM_OF_INTERFACES];
 extern TpSocket tpSocket1;
-extern TpSocket tpSocket2;
-extern TpSocket tpSocket3;
-extern TpSocket tpSocket4;
+extern InterfaceControl_SFBusMaster interfaceControlMaster1;
 void Interfaces_Init(uint16_t address);
 
-#endif
